4f610b38ac025f06e8d6a1f00f5bdfa17db2e352
Albert Ragány-Németh <raganyalbi@gmail.com>
2023-03-28 20:50:38 +0200

    Add basic card saving

---
+++ Menza.Client/CardService.cs
@@ -0,0 +1,23 @@
+﻿using Blazored.LocalStorage;
+
+namespace Menza.Client;
+
+public class CardService
+{
+    private readonly ILocalStorageService _localStorageService;
+
+    public CardService(ILocalStorageService localStorageService) => _localStorageService = localStorageService;
+
+    public ulong? CardNumber { get; private set; }
+    
+    public async Task Initialize()
+    {
+        CardNumber = await _localStorageService.GetItemAsync<ulong?>(nameof(CardNumber));
+    }
+ 
+    public async Task SetCardNumber(ulong? cardNumber)
+    {
+        CardNumber = cardNumber;
+        await _localStorageService.SetItemAsync(nameof(CardNumber), cardNumber);
+    }
+}
--- Menza.Client/Program.cs
+++ Menza.Client/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddSingleton<AuthService>();
 builder.Services.AddSingleton<IRepository, Repository>();
 builder.Services.AddSingleton<StartupService>();
 builder.Services.AddSingleton(_ => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<CardService>();
 
 CultureInfo.DefaultThreadCurrentCulture = new("hu-HU");
 CultureInfo.DefaultThreadCurrentUICulture = new("hu-HU");
